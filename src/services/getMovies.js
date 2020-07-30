49dc9faf0e992ff2699f34e3210587159f9731a4
Oscar Fuentes <ofuentese@gmail.com>
2020-07-30 17:52:08 -0600

    agregado page a la query a  la api

--- src/services/getMovies.js
+++ src/services/getMovies.js
@@ -1,7 +1,7 @@
 import baseUrl from "./urls";
 
-const getMovies = async (keyword) => {
-    const resp = await fetch(`${baseUrl}${keyword}`);
+const getMovies = async (keyword,page = "1") => {
+    const resp = await fetch(`${baseUrl}${keyword}&page=${page}`);
     const resJson = await resp.json();
     return resJson.results
 };
